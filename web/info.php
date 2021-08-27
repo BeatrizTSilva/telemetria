4ecc0283cbaa70f1be025ecb90becd9cef573109
Employee #427 <chcolatemousse@gmail.com>
2021-08-27 12:09:08 +0100

    ajax works if you open the connection to the databse directly on the info.php file

--- web/info.php
+++ web/info.php
@@ -5,9 +5,22 @@
  * Description: tests with ajax and real time updates
  *
 -------------------------------------------------------------------------------- */
+include ("file.php");
 
+$hostname = host();
+$username = username();
+$password = database_password();
+$database_name = database_name();
+$port = 5432;
+
+$database = pg_connect('host='.$hostname.' port='.$port.' dbname='.$database_name.' user='.$username.' password='.$password);
+if ($database) {
+    echo "Connection successful <br>";
+} else {
+    echo "Connection failed <br>";
+}
 /* ----------------------------------- 6th AJAX test --------------------------------- */
-/*$database = connectDB();
+//$database = connectDB();
 $result = pg_query($database,"SELECT * FROM telemetria ORDER BY t");
 
 $data = array();
@@ -16,11 +29,10 @@ while ($row = pg_fetch_assoc($result))
     array_push($data, $row);
 }
 
-echo "before json encode";
 echo json_encode($data);
-//exit();
-closeConnection($database);*/
+exit();
+//closeConnection($database);
 
-echo "Hello";
+//echo "Hello";
 
 ?>

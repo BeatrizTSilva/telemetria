3cef6f33d79d9a0e2759d87dab0f38f51433bbf4
Employee #427 <chcolatemousse@gmail.com>
2021-09-06 16:56:56 +0100

    added github button

--- web/index.php
+++ web/index.php
@@ -17,7 +17,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0">
 
-	<!---------------------------- for graphs --------------------------------->
+	<!-- for the GitHub icon -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!---------------------------- for the graphs --------------------------------->
 	<script src="https://code.highcharts.com/highcharts.js"></script>
 	<script src="https://code.highcharts.com/highcharts-more.js"></script>
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
@@ -25,13 +28,14 @@
 	<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 	<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
 
+	<!-------------------------- jquery (for ajax) ------------------------------>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 	<!----------------------------- includes ----------------------------------->
 	<?php
 	require("database-connection.php");
 	require("functions.php");
 	?>
-	<!-------------------------- jquery (for ajax) ------------------------------>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -40,12 +44,16 @@
 		<div id="clockbox">
 			<script src="clock.js"></script>
 		</div>
-		<p id="github-code">
+		<!--p id="github-code">
         	Code <a href="https://github.com/BeatrizTSilva/telemetria" target="_blank" rel="noopenernoreferrer">here</a>
-      	</p>
+      	</p-->
+		<a href="https://github.com/BeatrizTSilva/telemetria" target="_blank">
+        	<div class="github-button"> <p>Code</p> <i class="fa fa-github"></i> </div>
+        </a>
 	</div>
 
 
+	<!-- button to go to web design page -->
 	<!--a class="button-web-design" href="web-design/web-index.php" target="_blank">Go to Web Design</a-->
 
 	<!-- ----------------------------------------------- DATABASE ---------------------------------------------------------- -->
@@ -135,17 +143,17 @@
 	<div class="graphs-container">
 	<div class="splines-container">
 		<!--div class="highcharts-figure"-->
-			<div id="voltage-graph" class="one-graph"></div>
-			<div id="current-graph" class="one-graph"></div>
-			<div id="temperature-graph" class="one-graph"></div>
+			<div id="voltage-graph" class="highcharts-figure one-graph"></div>
+			<div id="current-graph" class="highcharts-figure one-graph"></div>
+			<div id="temperature-graph" class="highcharts-figure one-graph"></div>
 		<!--/div-->
 	</div>
 
 	<div class="gauges-container">
 		<!--div class="highcharts-figure"-->
-		<div id="speed-graph" class="one-graph"></div>
-		<div id="container-speed" class="gauge one-graph"></div>
-		<div id="container-rpm" class="gauge one-graph"></div>
+		<div id="speed-graph" class="highcharts-figure one-graph"></div>
+		<div id="container-speed" class="highcharts-figure gauge one-graph"></div>
+		<div id="container-rpm" class="highcharts-figure gauge one-graph"></div>
 		<!--/div-->
 	</div>
 

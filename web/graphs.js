757e4bec8ba1ae35c76eb5261e4bd818d3501772
Employee #427 <chcolatemousse@gmail.com>
2021-09-08 15:21:44 +0100

    before changing everything

--- web/graphs.js
+++ web/graphs.js
@@ -6,18 +6,15 @@
 *
 *****************************************************************************/
 
-/* -------------------------------- AJAX ORIGINAL -----------------------*/
-/*let ajax = new XMLHttpRequest();
-ajax.open("GET", "ajax.php", true);
-ajax.send();
 
-ajax.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log("we are readystatechnage");
-    let data = JSON.parse(this.responseText);
-    console.log(data);
-  }
-};*/
+/* --------------------- TO DO ---------------------- */
+/*
+xttp.send() to send back parameter T
+no setInterval inside graph
+the updates are dictated by going to the database
+if v[] == null -> no plot
+
+*/
 
 /* -------------------------------- AJAX CALL -------------------------------------*/
 
@@ -35,7 +32,7 @@ function call_ajax (parameter) {
       console.log(value_from_database);
       console.log("INSIDE call_ajax parameter = " + parameter);
 
-      y = 40;
+      //y = 40;
       switch(parameter) { // switch to return the right value to the right graph
         case 'temperature':
         y_string = value_from_database[counter].temperature; // value_from_database[counter].parameter will return a string
@@ -90,7 +87,7 @@ Highcharts.chart('temperature-graph', {
         let y_axis;
         setInterval(function () { // function that updates each 1000ms
           let x = (new Date()).getTime(); // x axis
-          y_axis = call_ajax(sensor); // will return the value of y from the corresponding sensor we want
+          y_axis = call_ajax('temperature'); // will return the value of y from the corresponding sensor we want
           series.addPoint([x, y_axis], true, true); // updates the graph
           console.log("INSIDE THE GRAPH Y = " + y_axis);
         }, 1000);
@@ -134,151 +131,151 @@ Highcharts.chart('temperature-graph', {
 
 
 /* ---------------------------------------- VOLTAGE ------------------------------------- */
-// Highcharts.chart('voltage-graph', {
-//   chart: {
-//       type: 'spline',
-//       // backgroundColor: '#808080',
-//       animation: Highcharts.svg, // don't animate in old IE
-//       marginRight: 10,
-//       events: {
-//         load: function () {
-//           // set up the updating of the chart each second
-//           let series = this.series[0];
-//           let counter = 0;
-//           setInterval(function () {
-//             let xhttp = new XMLHttpRequest();
-//             xhttp.onreadystatechange = function () {
-//               if (this.readyState == 4 && this.status == 200) {
-//                 let x = (new Date()).getTime(); // x axis
-//                 let y; // y axis
-//                 let y_string; // aux variable
-//                 let value_from_database = JSON.parse(this.responseText); // everything that comes from the database (in ajax.php)
+Highcharts.chart('voltage-graph', {
+  chart: {
+      type: 'spline',
+      // backgroundColor: '#808080',
+      animation: Highcharts.svg, // don't animate in old IE
+      marginRight: 10,
+      events: {
+        load: function () {
+          // set up the updating of the chart each second
+          let series = this.series[0];
+          let counter = 0;
+          setInterval(function () {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+              if (this.readyState == 4 && this.status == 200) {
+                let x = (new Date()).getTime(); // x axis
+                let y; // y axis
+                let y_string; // aux variable
+                let value_from_database = JSON.parse(this.responseText); // everything that comes from the database (in ajax.php)
 
-//                 console.log("this.Response in voltage is " + this.responseText);
-//                 console.log(value_from_database);
+                console.log("this.Response in voltage is " + this.responseText);
+                console.log(value_from_database);
 
-//                 y_string = value_from_database[counter].voltage; // data[0].voltage will return a string
-//                 y = parseFloat(y_string); // make the string a float
+                y_string = value_from_database[counter].voltage; // data[0].voltage will return a string
+                y = parseFloat(y_string); // make the string a float
 
-//                 series.addPoint([x, y], true, true); // updates the graph
+                series.addPoint([x, y], true, true); // updates the graph
 
-//                 counter++; // increase counter to go to nextvalue in time
-//               }
-//             };
-//             xhttp.open("GET", "ajax.php", true); //go get stuff from ajax.php
-//             xhttp.send();
-//           }, 1000);
-//         }
-//       }
-//     },
-//   time: { useUTC: false },
-//   title: { text: 'Voltage' },
-//   accessibility: {
-//       announceNewData: { enabled: true, minAnnounceInterval: 15000,
-//           announcementFormatter: function (allSeries, newSeries, newPoint) {
-//               if (newPoint) { return 'New point added. Value: ' + newPoint.y; }
-//               return false;
-//           }
-//       }
-//   },
-//   xAxis: { type: 'datetime', tickPixelInterval: 150 },
-//   yAxis: {
-//     title: { text: 'Voltage [V]' },
-//     // plotLines: [{ value: 0, width: 1, color: '#808080'}]
-//     plotLines: [{ value: 0, width: 1, color: '#89c45f'}]
-//   },
-//   tooltip: {
-//       headerFormat: '<b>{series.name}</b><br/>',
-//       pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-//   },
-//   legend: { enabled: false }, exporting: { enabled: false },
-//   series: [{
-//       name: 'Voltage',
-//       data: (function () { let data = [], time = (new Date()).getTime(), i;
-//           for (i = -30; i <= 0; i += 1) {
-//               data.push({
-//                   x: time + i * 1000, // shows time in a comprehensible way
-//                   y:0 // initial value (was "y: Math.random()")
-//               });
-//           }
-//           return data;
-//       }())
-//   }]
-// });
+                counter++; // increase counter to go to nextvalue in time
+              }
+            };
+            xhttp.open("POST", "ajax.php", true); //go get stuff from ajax.php
+            xhttp.send();
+          }, 1000);
+        }
+      }
+    },
+  time: { useUTC: false },
+  title: { text: 'Voltage' },
+  accessibility: {
+      announceNewData: { enabled: true, minAnnounceInterval: 15000,
+          announcementFormatter: function (allSeries, newSeries, newPoint) {
+              if (newPoint) { return 'New point added. Value: ' + newPoint.y; }
+              return false;
+          }
+      }
+  },
+  xAxis: { type: 'datetime', tickPixelInterval: 150 },
+  yAxis: {
+    title: { text: 'Voltage [V]' },
+    // plotLines: [{ value: 0, width: 1, color: '#808080'}]
+    plotLines: [{ value: 0, width: 1, color: '#89c45f'}]
+  },
+  tooltip: {
+      headerFormat: '<b>{series.name}</b><br/>',
+      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+  },
+  legend: { enabled: false }, exporting: { enabled: false },
+  series: [{
+      name: 'Voltage',
+      data: (function () { let data = [], time = (new Date()).getTime(), i;
+          for (i = -30; i <= 0; i += 1) {
+              data.push({
+                  x: time + i * 1000, // shows time in a comprehensible way
+                  y:0 // initial value (was "y: Math.random()")
+              });
+          }
+          return data;
+      }())
+  }]
+});
 
 
 /* ---------------------------------------- CURRENT ------------------------------------- */
-// Highcharts.chart('current-graph', {
-//   chart: {
-//       type: 'spline',
-//       // backgroundColor: '#808080',
-//       animation: Highcharts.svg, // don't animate in old IE
-//       marginRight: 10,
-//       events: {
-//         load: function () {
-//           // set up the updating of the chart each second
-//           let series = this.series[0];
-//           let counter = 0;
-//           setInterval(function () {
-//             let xhttp = new XMLHttpRequest();
-//             xhttp.onreadystatechange = function () {
-//               if (this.readyState == 4 && this.status == 200) {
-//                 let x = (new Date()).getTime(); // x axis
-//                 let y; // y axis
-//                 let y_string; // aux variable
-//                 let value_from_database = JSON.parse(this.responseText); // everything that comes from the database (in ajax.php)
+Highcharts.chart('current-graph', {
+  chart: {
+      type: 'spline',
+      // backgroundColor: '#808080',
+      animation: Highcharts.svg, // don't animate in old IE
+      marginRight: 10,
+      events: {
+        load: function () {
+          // set up the updating of the chart each second
+          let series = this.series[0];
+          let counter = 0;
+          setInterval(function () {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+              if (this.readyState == 4 && this.status == 200) {
+                let x = (new Date()).getTime(); // x axis
+                let y; // y axis
+                let y_string; // aux variable
+                let value_from_database = JSON.parse(this.responseText); // everything that comes from the database (in ajax.php)
 
-//                 console.log("this.Response in current is " + this.responseText);
-//                 console.log(value_from_database);
+                console.log("this.Response in current is " + this.responseText);
+                console.log(value_from_database);
 
-//                 y_string = value_from_database[counter].current; // data[0].voltage will return a string
-//                 y = parseFloat(y_string); // make the string a float
+                y_string = value_from_database[counter].current; // data[0].voltage will return a string
+                y = parseFloat(y_string); // make the string a float
 
-//                 series.addPoint([x, y], true, true); // updates the graph
+                series.addPoint([x, y], true, true); // updates the graph
 
-//                 counter++; // increase counter to go to nextvalue in time
-//               }
-//             };
-//             xhttp.open("GET", "ajax.php", true); //go get stuff from ajax.php
-//             xhttp.send();
-//           }, 1000);
-//         }
-//       }
-//     },
-//   time: { useUTC: false },
-//   title: { text: 'Current' },
-//   accessibility: {
-//       announceNewData: { enabled: true, minAnnounceInterval: 15000,
-//           announcementFormatter: function (allSeries, newSeries, newPoint) {
-//               if (newPoint) { return 'New point added. Value: ' + newPoint.y; }
-//               return false;
-//           }
-//       }
-//   },
-//   xAxis: { type: 'datetime', tickPixelInterval: 150 },
-//   yAxis: {
-//     title: { text: 'Voltage [V]' },
-//     // plotLines: [{ value: 0, width: 1, color: '#808080'}]
-//     plotLines: [{ value: 0, width: 1, color: '#89c45f'}]
-//   },
-//   tooltip: {
-//       headerFormat: '<b>{series.name}</b><br/>',
-//       pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-//   },
-//   legend: { enabled: false }, exporting: { enabled: false },
-//   series: [{
-//       name: 'Voltage',
-//       data: (function () { let data = [], time = (new Date()).getTime(), i;
-//           for (i = -30; i <= 0; i += 1) {
-//               data.push({
-//                   x: time + i * 1000, // shows time in a comprehensible way
-//                   y:0 // initial value (was "y: Math.random()")
-//               });
-//           }
-//           return data;
-//       }())
-//   }]
-// });
+                counter++; // increase counter to go to nextvalue in time
+              }
+            };
+            xhttp.open("GET", "ajax.php", true); //go get stuff from ajax.php
+            xhttp.send();
+          }, 1000);
+        }
+      }
+    },
+  time: { useUTC: false },
+  title: { text: 'Current' },
+  accessibility: {
+      announceNewData: { enabled: true, minAnnounceInterval: 15000,
+          announcementFormatter: function (allSeries, newSeries, newPoint) {
+              if (newPoint) { return 'New point added. Value: ' + newPoint.y; }
+              return false;
+          }
+      }
+  },
+  xAxis: { type: 'datetime', tickPixelInterval: 150 },
+  yAxis: {
+    title: { text: 'Voltage [V]' },
+    // plotLines: [{ value: 0, width: 1, color: '#808080'}]
+    plotLines: [{ value: 0, width: 1, color: '#89c45f'}]
+  },
+  tooltip: {
+      headerFormat: '<b>{series.name}</b><br/>',
+      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+  },
+  legend: { enabled: false }, exporting: { enabled: false },
+  series: [{
+      name: 'Voltage',
+      data: (function () { let data = [], time = (new Date()).getTime(), i;
+          for (i = -30; i <= 0; i += 1) {
+              data.push({
+                  x: time + i * 1000, // shows time in a comprehensible way
+                  y:0 // initial value (was "y: Math.random()")
+              });
+          }
+          return data;
+      }())
+  }]
+});
 
 
 /* ---------------------------------------- TEMPERATURE ------------------------------------- */
@@ -605,6 +602,8 @@ function (chart) {
 
 /* function for console logging values */
 setInterval(function () { // function that updates each 1000ms
+  let p;
+  p = call_ajax('temperature');
   console.log("LATE OUTSIDE call_ajax counter = " + counter);
-  console.log("LATE OUTSIDE call_ajax y = " + y);
+  console.log("LATE OUTSIDE call_ajax y = " + p);
 }, 1000);

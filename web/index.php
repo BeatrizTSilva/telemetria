e71377e9b9d868419345a4fdde14adc82a7d3d80
Employee #427 <chcolatemousse@gmail.com>
2021-09-15 18:13:02 +0100

    button for psem's website working

--- web/index.php
+++ web/index.php
@@ -45,7 +45,7 @@
 			<script src="clock.js"></script>
 		</div>
 		<!-- button to go to web design page -->
-		<a class="button-web-design" href="web-design/web-index.php" target="_blank">Go to Web Design</a>
+		<a class="button-web-design" href="psem-website/index.html" target="_blank">Website</a>
 
 		<a href="https://github.com/BeatrizTSilva/telemetria" target="_blank">
         	<div class="github-button"> <!--p>Code</p--> <i class="fa fa-github"></i> </div>
